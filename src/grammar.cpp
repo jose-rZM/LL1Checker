0b74ef0b447195eed1831718390c46eecaccd132
Jose <github@altdom.simplelogin.com>
2024-02-10 16:24:02 +0100

    added second constructor, moved reading grammar to grammar struct

--- src/grammar.cpp
+++ src/grammar.cpp
@@ -1,8 +1,61 @@
 #include "grammar.hpp"
 #include "symbol_table.hpp"
+#include <fstream>
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <regex>
+
+grammar::grammar(const std::string& filename) : filename_(filename) {
+  read_from_file();
+}
+
+void grammar::read_from_file() {
+  const std::string filename{"input.txt"};
+  std::ifstream file;
+  file.open(filename, std::ios::in);
+  if (file.is_open()) {
+    std::string input;
+    std::smatch match;
+    while (getline(file, input) && input != ";") {
+      std::string id;
+      std::string value;
+      std::regex rx_no_terminal{
+          "no\\s+terminal\\s+([a-zA-Z_\\'][a-zA-Z_0-9\\']*);"};
+      std::regex rx_terminal{
+          "terminal\\s+([a-zA-Z_\\'][a-zA-Z_0-9\\']*)\\s+([^]*);"};
+      std::regex rx_axiom{"start\\s+with\\s+([a-zA-Z_\\'][a-zA-Z_0-9\\']*);"};
+      if (std::regex_match(input, match, rx_no_terminal)) {
+        symbol_table::put_symbol(match[1], NO_TERMINAL);
+      } else if (std::regex_match(input, match, rx_terminal)) {
+        symbol_table::put_symbol(match[1], TERMINAL, match[2]);
+      } else if (std::regex_match(input, match, rx_axiom)) {
+        set_axiom(match[1]);
+      } else {
+        std::cout << "Error while reading tokens: " << input << "\n";
+        exit(-1);
+      }
+    }
+
+    while (getline(file, input) && input != ";") {
+      std::regex rx_empty_production{"([a-zA-Z_\\'][a-zA-Z_0-9\\']*)\\s*->;"};
+      std::regex rx_production{"([a-zA-Z_\\'][a-zA-Z_0-9\\']*)\\s*->\\s*([a-zA-"
+                               "Z_\\'][a-zA-Z_0-9\\s$\\']*);"};
+      if (std::regex_match(input, match, rx_production)) {
+        std::string s = match[2];
+        s.erase(std::remove_if(s.begin(), s.end(), ::isspace), s.end());
+        add_rule(match[1], s);
+      } else if (std::regex_match(input, match, rx_empty_production)) {
+        add_rule(match[1], symbol_table::EPSILON);
+      } else {
+        std::cout << "Error while reading grammar: " << input << "\n";
+        exit(-1);
+      }
+    }
+  }
+
+  file.close();
+}
 
 std::vector<std::string> grammar::split(const std::string &s) {
   if (s == symbol_table::EPSILON) {
