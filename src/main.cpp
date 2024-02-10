9b105a1ce115e271f81c4485d5471ef4dcf38da3
Jose <github@altdom.simplelogin.com>
2024-02-10 17:56:48 +0100

    grammar debug when error added

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,8 @@
-#include "grammar.hpp"
 #include "lexer.hpp"
-#include "symbol_table.hpp"
-#include <algorithm>
-#include <cstdio>
 #include <fstream>
-#include <ios>
 #include <iostream>
-#include <istream>
-#include <iterator>
-#include <map>
 #include <ostream>
-#include <regex>
-#include <stack>
-#include <stdexcept>
 #include <string>
-#include <sys/types.h>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
-#include <dlfcn.h>
 #include "ll1_parser.hpp"
 
 
