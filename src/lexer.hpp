fa4660c854cc342c232eddcfdd8d1c009f9b35fd
Jose <github@altdom.simplelogin.com>
2024-02-11 15:11:08 +0100

    documented lexer

--- src/lexer.hpp
+++ src/lexer.hpp
@@ -1,11 +1,18 @@
 #include <fstream>
 #include <iterator>
 #include <vector>
-struct lexer {
+class lexer {
     std::string filename_;
     std::vector<std::string> tokens_;
     unsigned current_;
+    public:
     lexer(const std::string &filename);
     std::string next();
+  private:
     void tokenize();
+    void make_lexer();
+    void compile();
+    const std::string LEXER_FILENAME {"lexer.l"};
+    const std::string SRC_PATH {"./src"};
+    const std::string LIB_PATH {"./lib"};
 };
