f192913d443a81e136330060ea81317198ebdfa5
jose-rZM <100773386+jose-rZM@users.noreply.github.com>
2025-06-22 13:04:50 +0200

    feat: remove use of boost spirit

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,61 +1,83 @@
 #include "../include/lexer.hpp"
 #include "../include/lexer_error.hpp"
 #include "../include/symbol_table.hpp"
-#include <boost/bind/bind.hpp>
-#include <boost/ref.hpp>
-#include <boost/spirit/include/lex_lexertl.hpp>
 #include <cstdlib>
 #include <cstring>
 #include <fstream>
 #include <iostream>
-#include <string>
+#include <regex>
+#include <string_view>
+
+namespace {
+std::string EscapeRegex(const std::string& str) {
+    static const std::regex special{R"([.^$|()\[\]{}*+?\\])"};
+    return std::regex_replace(str, special, R"(\$&)");
+}
+
+struct Pattern {
+    std::string type;
+    std::regex  regex;
+};
+} // namespace
 
 Lex::Lex(std::string filename) : filename_(std::move(filename)), current_() {
     Tokenize();
 }
 
-template <typename Lexer> Lex::ParseInput<Lexer>::ParseInput() {
-    unsigned long i{1};
-    std::string   token_type{symbol_table::token_types_r_.at(i)};
-    this->self.add("\\" + symbol_table::EOL_, i);
-    ++i;
-    for (size_t j = 1; j < symbol_table::order_.size(); ++j) {
-        token_type = symbol_table::token_types_r_.at(i);
-        std::string regex{symbol_table::st_.at(token_type).second};
-        this->self.add(regex, i);
-        ++i;
-    }
-    this->self.add("[ \\t\\n]+", i);
-}
-
-template <typename Token>
-bool Lex::Add::operator()(Token const& t, std::vector<std::string>& tks) const {
-    if (static_cast<unsigned long>(t.id()) == symbol_table::i_) {
-        return true;
-    }
-    tks.push_back(
-        symbol_table::token_types_r_.at(static_cast<unsigned long>(t.id())));
-    return true;
-}
-
 void Lex::Tokenize() {
-    ParseInput<boost::spirit::lex::lexertl::lexer<>> functor;
-    using boost::placeholders::_1;
-    std::ifstream      file(filename_);
+    std::ifstream file(filename_);
+    if (!file) {
+        throw LexerError("Cannot open " + filename_);
+    }
     std::ostringstream buffer;
     buffer << file.rdbuf();
-    std::string input     = buffer.str();
-    char const* first     = input.c_str();
-    char const* end       = &first[input.size()];
-    bool        completed = boost::spirit::lex::tokenize(
-        first, end, functor, boost::bind(Add(), _1, boost::ref(tokens_)));
-    if (!completed) {
-        std::string rest(first, end);
-        throw LexerError("Lexical error: encountered an invalid token:\n" +
-                         rest);
+    std::string input = buffer.str();
+    size_t      pos   = 0;
+
+    std::vector<Pattern> patterns;
+    patterns.reserve(symbol_table::order_.size());
+
+    patterns.push_back({symbol_table::token_types_r_.at(1),
+                        std::regex(EscapeRegex(symbol_table::EOL_))});
+
+    for (size_t j = 1; j < symbol_table::order_.size(); ++j) {
+        unsigned long id         = symbol_table::order_[j];
+        std::string   token_type = symbol_table::token_types_r_.at(id);
+        std::string   regex_str  = symbol_table::st_.at(token_type).second;
+        patterns.emplace_back(token_type, std::regex(regex_str));
+    }
+
+    std::regex ws{R"([ \t\n]+)"};
+    while (pos < input.size()) {
+        std::string_view remaining{input.data() + pos, input.size() - pos};
+        std::cmatch      m;
+        if (std::regex_search(remaining.begin(), remaining.end(), m, ws,
+                              std::regex_constants::match_continuous)) {
+            pos += m.length();
+            continue;
+        }
+        size_t      best_len = 0;
+        std::string best_tok;
+        for (const auto& p : patterns) {
+            if (std::regex_search(remaining.begin(), remaining.end(), m,
+                                  p.regex,
+                                  std::regex_constants::match_continuous)) {
+                if (m.length() > best_len) {
+                    best_len = m.length();
+                    best_tok = p.type;
+                }
+            }
+        }
+        if (best_len == 0) {
+            std::string rest(remaining.begin(), remaining.end());
+            throw LexerError("Lexical error: encountered an invalid token:\n" +
+                             rest);
+        }
+        tokens_.push_back(best_tok);
+        pos += best_len;
     }
 }
 
 std::string Lex::Next() {
-    return current_ >= tokens_.size() ? "" : tokens_[current_++];
+    return current_ >= tokens_.size() ? std::string() : tokens_[current_++];
 }
