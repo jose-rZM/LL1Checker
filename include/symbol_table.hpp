61dbeadcef855c4399b492d1e702c41c74191a31
jose-rZM <100773386+jose-rZM@users.noreply.github.com>
2025-06-24 12:14:54 +0200

    refactor(symbol table): change eol symbol

--- include/symbol_table.hpp
+++ include/symbol_table.hpp
@@ -8,7 +8,7 @@ enum symbol_type { NO_TERMINAL, TERMINAL };
 
 struct symbol_table {
     /// @brief End-of-line symbol used in parsing, initialized as "$".
-    inline static std::string EOL_{"$"};
+    inline static std::string EOL_{"EOF"};
 
     /// @brief Epsilon symbol, representing empty transitions, initialized as
     /// "EPSILON".
@@ -18,7 +18,7 @@ struct symbol_table {
     /// and its regex.
     inline static std::unordered_map<std::string,
                                      std::pair<symbol_type, std::string>>
-        st_{{EOL_, {TERMINAL, EOL_}}, {EPSILON_, {TERMINAL, EPSILON_}}};
+        st_{{EOL_, {TERMINAL, ""}}, {EPSILON_, {TERMINAL, EPSILON_}}};
 
     /// @brief Token types, mapping each symbol to a unique integer ID.
     inline static std::unordered_map<std::string, unsigned long> token_types_{
