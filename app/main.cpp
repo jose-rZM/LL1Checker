6ebdbe11dc40c80b929454aa9e730c2f1dc26ba7
jose-rZM <100773386+jose-rZM@users.noreply.github.com>
2025-06-25 22:38:13 +0200

    refactor: improve msgs

--- app/main.cpp
+++ app/main.cpp
@@ -43,7 +43,7 @@ int main(int argc, char* argv[]) {
             cxxopts::value<std::string>(grammar_filename))(
             "text", "Text file to parse",
             cxxopts::value<std::string>(text_filename)->default_value(""))(
-            "export-tree", "Export parse tree to file",
+            "export-tree", "Export parse tree to dot file. Filename is mandatory",
             cxxopts::value<std::string>(export_tree_file));
 
         options.parse_positional({"grammar", "text"});
@@ -117,8 +117,8 @@ int main(int argc, char* argv[]) {
                 if (!tree) {
                     return 1;
                 }
-                std::cout << "Parsing successful. Parse tree exported! Run "
-                             "'dot' to generate an image\n";
+                std::cout << "Parsing successful.\nParse tree exported! Run "
+                             "'dot -Tpng " + export_tree_file + " <output.png>' to generate an image\n";
                 parser.ExportTreeAsDot(tree, export_tree_file);
             } else {
                 if (!parser.Parse()) {
